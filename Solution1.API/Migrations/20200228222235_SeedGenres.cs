dd792e56d3e3e3dd888700f3e757b3e08a36b1a8
Pavel <pavel.ivanko@hotmail.com>
2020-02-29 09:29:00 +0100

    Seeded genres and generated MVC front

---
+++ Solution1.API/Migrations/20200228222235_SeedGenres.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Solution1.API.Migrations
+{
+    public partial class SeedGenres : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Weapon')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Vehicle')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Book')");
+            migrationBuilder.Sql("INSERT INTO Genres (Name) VALUES ('Other')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Genres");
+        }
+    }
+}
